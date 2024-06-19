209f18558c298ceb547a3f1dd38d3d120f01909d
dalim <dalimabrouk2003@gmail.com>
2024-06-19 21:26:35 +0200

    bouton valider ue

--- back/details-UE/routes-details-UE.js
+++ back/details-UE/routes-details-UE.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router= express.Router()
 const pool = require('../connexion-bd.js');
 const { getDetailsUE } = require("./bd-details-UE");
+const { validerUE } = require('./bd-details-UE');
+
 
 // Route spécifique à une UE, pour obtenir ses détails
 router.get("/api/ue/:idUE",(req,res)=>{
@@ -14,6 +16,13 @@ router.get("/api/ue/:idUE",(req,res)=>{
         res.json(result);
     });
 
+});
+
+router.post('/validerUE', (req, res) => {
+    const { etudiantId, ueId } = req.body;
+    validerUE(etudiantId, ueId)
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(500).json({ error: err.message }));
 });
 
 
