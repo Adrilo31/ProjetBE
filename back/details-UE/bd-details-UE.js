a2c52a6c634fb4bb377a8fd600fb1247243a0ccf
MelaniePignon <melaniepignon@outlook.com>
2024-06-20 11:46:47 +0200

    modif details UE

--- back/details-UE/bd-details-UE.js
+++ back/details-UE/bd-details-UE.js
@@ -1,36 +1,71 @@
-
-// Requête pour récupérer :
-// - Des infos sur l'UE :
-// * nom,
+// Requête pour récupérer des infos sur l'UE :
+// * code, nom,
 // * mention associée,
 // * taux de réussite
-// - La liste des étudiants liés à l'UE et leurs infos :
-// * numéro étudiant,
-// * prénom,
-// * nom,
-// * année universitaire,
-// * semestre,
-// * si validé ou non
 function getDetailsUE() {
     return `
-        with infosUE as (
-            select code, nomUE, nomMen, sum(suivre.valide)/count(suivre.etudiant_id) taux
-            from unitesenseignement inner join mentions on unitesenseignement.mention_id = mentions.idMen
-            inner join suivre on unitesenseignement.code = suivre.ue_code
-            where code = ?
-            group by code)
-        select infosUE.code, infosUE.nomUE, infosUE.nomMen, infosUE.taux, 
-        IdEtu, prenomEtu, nomEtu, concat(annee_debut, ' - ', annee_fin) annee, nomSem, valide 
+        select code, nomUE, nomMen, sum(suivre.valide)/count(suivre.etudiant_id) taux
+        from unitesenseignement inner join mentions on unitesenseignement.mention_id = mentions.idMen
+        inner join suivre on unitesenseignement.code = suivre.ue_code
+        inner join semestres on suivre.semestre_id = semestres.idSem
+        where code = ?
+        group by code;
+    `
+}
+
+// Requête pour récupérer la liste des étudiants inscrits à l'UE :
+// * numéro étudiant,
+// * prénom, nom,
+// * année universitaire,
+// * semestre, si en cours,
+// * si validé ou non,
+function getListeInscritsUE() {
+    return `
+        select IdEtu, prenomEtu, nomEtu, CONCAT(annee_debut, ' - ', annee_fin) annee, nomSem, valide, en_cours 
         from etudiants inner join suivre on etudiants.IdEtu = suivre.etudiant_id
         inner join unitesenseignement on suivre.ue_code = unitesenseignement.code
         inner join semestres on suivre.semestre_id = semestres.idSem
         inner join annee on semestres.annee_id = annee.idAnn
         inner join mentions on unitesenseignement.mention_id = mentions.idMen
-        inner join infosUE on unitesenseignement.code = infosUE.code
         where ue_code = ?
         order by nomEtu, prenomEtu ASC;
     `
 }
+
+
+// Requête pour récupérer la liste des étudiants inscrits à l'UE pour le semestre en cours :
+// * numéro étudiant,
+// * prénom, nom,
+// * si validé ou non,
+// * numéro du semestre
+function getListeEtuEnCours() {
+    return `
+        select IdEtu, prenomEtu, nomEtu, valide, semestre_id
+        from etudiants inner join suivre on etudiants.IdEtu = suivre.etudiant_id
+        inner join semestres on suivre.semestre_id = semestres.idSem
+        where ue_code = ?
+        and en_cours
+        order by nomEtu, prenomEtu ASC;
+    `
+}
+
+
+// Requête pour récupérer :
+// - La liste des étudiants pouvant s'inscrire à l'UE (et qui ne l'ont pas déjà validée) :
+// * numéro étudiant,
+// * prénom, nom
+function getListeInscrire() {
+    return `
+        select distinct IdEtu, prenomEtu, nomEtu
+        from etudiants
+        where IdEtu not in (select etudiant_id from suivre
+                            where ue_code = ? and valide = true)
+        and (select count(*) from etreprerequis
+        left join suivre on etreprerequis.ue_prerequise_code = suivre.ue_code and suivre.etudiant_id = etudiants.IdEtu and valide = true
+        where ue_visee_code = ? and etudiant_id is null) = 0;
+    `
+}
+
 
 
 const { db } = require('../connexion-bd');
@@ -49,4 +84,5 @@ module.exports = {
     validerUE,
 };
 
-module.exports = { getDetailsUE };
+
+module.exports = { getDetailsUE, getListeInscritsUE, getListeEtuEnCours, getListeInscrire};
--- back/details-UE/routes-details-UE.js
+++ back/details-UE/routes-details-UE.js
@@ -1,23 +1,52 @@
 const express = require('express');
 const router= express.Router()
 const pool = require('../connexion-bd.js');
-const { getDetailsUE } = require("./bd-details-UE");
+const { getDetailsUE, getListeInscritsUE, getListeEtuEnCours, getListeInscrire } = require("./bd-details-UE");
 const { validerUE } = require('./bd-details-UE');
 
 
-// Route spécifique à une UE, pour obtenir ses détails
+// Route pour obtenir les détails d'une UE
 router.get("/api/ue/:idUE",(req,res)=>{
-
     const codeUE = req.params.idUE;
     const sql = getDetailsUE();
+    pool.query(sql, [codeUE], (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    });
+});
 
+// Route pour obtenir la liste des étudiants inscrits à l'UE
+router.get("/api/ue/:idUE/inscrits",(req,res)=>{
+    const codeUE = req.params.idUE;
+    const sql = getListeInscritsUE();
+    pool.query(sql, [codeUE], (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    });
+});
+
+// Route pour obtenir la liste des étudiants inscrits à l'UE pour le semestre en cours
+router.get("/api/ue/:idUE/valider",(req,res)=>{
+    const codeUE = req.params.idUE;
+    const sql = getListeEtuEnCours();
+    pool.query(sql, [codeUE], (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    });
+});
+
+// Route pour obtenir la liste des étudiants pouvant s'inscrire à l'UE
+router.get("/api/ue/:idUE/inscrire",(req,res)=>{
+    const codeUE = req.params.idUE;
+    const sql = getListeInscrire();
     pool.query(sql, [codeUE, codeUE], (err, result) => {
         if(err) throw err;
         res.json(result);
     });
-
 });
 
+
+// Route pour valider une UE
 router.post('/validerUE', (req, res) => {
     const { etudiantId, ueId } = req.body;
     validerUE(etudiantId, ueId)
