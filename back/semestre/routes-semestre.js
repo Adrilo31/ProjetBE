ed4db983cf12141de4586126535d163fc7eaadd5
MelaniePignon <melaniepignon@outlook.com>
2024-06-24 15:21:46 +0200

    modif route

--- back/semestre/routes-semestre.js
+++ back/semestre/routes-semestre.js
@@ -4,7 +4,7 @@ const router= express.Router()
 const pool = require('../connexion-bd.js');
 
 // Route pour obtenir le semestre en cours
-router.get("/api/semestre/actuel",(req,res)=>{
+router.get("/semestre/actuel",(req,res)=>{
     const sql = `select idSem, CONCAT(annee_debut, ' - ', annee_fin) annee, nomSem, en_cours
                         from semestres inner join annee on semestres.annee_id = annee.idAnn
                         where en_cours;`;
@@ -15,7 +15,7 @@ router.get("/api/semestre/actuel",(req,res)=>{
 });
 
 // Route pour obtenir le semestre suivant
-router.get("/api/semestre/suivant",(req,res)=>{
+router.get("/semestre/suivant",(req,res)=>{
     const sql = `select IFNULL(idSem, 'erreur') idSem, IFNULL(CONCAT(annee_debut, ' - ', annee_fin), 'erreur') annee, 
                         IFNULL(nomSem, 'erreur') nomSem, IFNULL(en_cours, 'erreur') en_cours
                         from (select idSem+1 idSemSuiv from semestres where en_cours) semestreSuivant
@@ -28,7 +28,7 @@ router.get("/api/semestre/suivant",(req,res)=>{
 });
 
 // Route pour obtenir le nombre d'étudiant qui n'ont pas validé par UE
-router.get("/api/semestre/pas-valide",(req,res)=>{
+router.get("/semestre/pas-valide",(req,res)=>{
     const sql = `select ue_code, nomUE, count(etudiant_id) nb_non_valide
                         from semestres inner join suivre on semestres.idSem = suivre.semestre_id
                         inner join unitesenseignement on suivre.ue_code = unitesenseignement.code
@@ -41,8 +41,8 @@ router.get("/api/semestre/pas-valide",(req,res)=>{
     });
 });
 
-// Route pour passer au semestre suivant
-router.put("/api/semestre/changer",(req,res)=>{
+// Route pour changer de semestre
+router.put("/semestre/changer",(req,res)=>{
     const { semestreActuel, semestreSuivant } = req.body;
     const sql = `update semestres
                         set en_cours = case
